69d62f8374be92477df24e4bfd4d9f153108571d
Gmero <2431775986@qq.com>
2024-03-21 15:16:56 +0800

    add target to css

--- src/modules/tinyCSS.ts
+++ src/modules/tinyCSS.ts
@@ -1,6 +1,12 @@
-import { transform } from 'lightningcss'
+import browserslist from 'browserslist'
+import { browserslistToTargets, transform } from 'lightningcss'
 import { minimatch } from 'minimatch'
 import TinyConfig from '../config'
+
+const targets = browserslistToTargets(
+  browserslist(['> 1%', 'last 2 versions', 'not dead']),
+)
+
 export default function tinyCSS(css: string, data: { path: string }) {
   if (TinyConfig.css.exclude.some((pattern) => minimatch(data.path, pattern)))
     return css
@@ -10,6 +16,7 @@ export default function tinyCSS(css: string, data: { path: string }) {
     code: Buffer.from(css),
     filename: 'style.css',
     sourceMap: false,
+    targets,
   })
 
   return code.toString()
