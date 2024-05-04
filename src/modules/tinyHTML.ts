49bb79b1c6cd492ae39be1a13e6ab5948c8254d5
Gmero <2431775986@qq.com>
2024-05-05 01:00:06 +0800

    use html-minifier to minify html

--- src/modules/tinyHTML.ts
+++ src/modules/tinyHTML.ts
@@ -1,9 +1,9 @@
-import minifyHtml from '@minify-html/node'
+import { minify } from 'html-minifier'
 import { minimatch } from 'minimatch'
 import TinyConfig from '../config'
 
 export default function tinyHTML(html: string, data: { path: string }) {
   if (TinyConfig.html.exclude.some((pattern) => minimatch(data.path, pattern)))
     return html
-  return minifyHtml.minify(Buffer.from(html), {})
+  return minify(html, {})
 }
